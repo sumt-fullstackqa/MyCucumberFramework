7e3b511484d3f0e21e1231a30b9d49353e0b5f8e
Ankur Bhardwaj <ankur.bhardwaj@cloudsmartz.net>
2020-06-11 15:29:51 +0530

    modified runner file

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -11,12 +11,14 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/resources/feature" }, tags = {"@Addcompany,"+ "@Interconnect,"+ "@Inventory,"+ "@Library,"+ "@Location,"+ "@AddNewBillingAccount,"+ "@AddNewCustomeruser,"+ "@NetworkTemplate,"+ "@AddNewuser,"+ "@AddRack,"+ "@ApproveandRejectCustomer,"+ "@AssignPort,"+ "@AssignRoletoCustomeruser,"+ "@CustomerRequestPort,"+ "@CustomerUserResetPassword,"+ "@DeactivateandActivateCustomerUser,"+ "@DeactivateandActivateUser,"+ "@EditCompany,"+ "@EditCustomerUser,"+ "@Edituser,"+ "@Exportfile,"+ "@LoginandLogout,"+ "@PortandElan,"+ "@PortandEline,"+ "@PortandETree,"+ "@RaiseNewTicket,"+ "@editticket,"+ "@Reopenclosedticket,"+ "@UserResetPassword,"+ "@viewnetworks,"+ "@Viewcompanydetails,"+ "@vieworderwithcustomer,"+ "@ViewTicketdetails,"+ "@ViewTicketReports"},
-                               glue = {"stepDefinition"},
-                               plugin = { "html:target/reports/cucumber-preety","json:target/cucumber.json",
-                            		       "com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html",  }, 
-
-                               monochrome = true, dryRun = false)
+@CucumberOptions(
+		features = { "src/test/resources/feature" }, 
+        glue = {"stepDefinition"},
+        monochrome = true,
+        plugin = { "html:target/reports/cucumber-preety","json:target/cucumber.json",
+                   "com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html",  }, 
+        tags = {"@Addcompany,"+ "@Interconnect,"+ "@Inventory,"+ "@Library,"+ "@Location,"+ "@AddNewBillingAccount,"+ "@AddNewCustomeruser,"+ "@NetworkTemplate,"+ "@AddNewuser,"+ "@AddRack,"+ "@ApproveandRejectCustomer,"+ "@AssignPort,"+ "@AssignRoletoCustomeruser,"+ "@CustomerRequestPort,"+ "@CustomerUserResetPassword,"+ "@DeactivateandActivateCustomerUser,"+ "@DeactivateandActivateUser,"+ "@EditCompany,"+ "@EditCustomerUser,"+ "@Edituser,"+ "@Exportfile,"+ "@LoginandLogout,"+ "@PortandElan,"+ "@PortandEline,"+ "@PortandETree,"+ "@RaiseNewTicket,"+ "@editticket,"+ "@Reopenclosedticket,"+ "@UserResetPassword,"+ "@viewnetworks,"+ "@Viewcompanydetails,"+ "@vieworderwithcustomer,"+ "@ViewTicketdetails,"+ "@ViewTicketReports"}
+ )
 
 public class Runner {
 	
